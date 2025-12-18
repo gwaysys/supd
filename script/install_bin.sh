409bd43743b6a33d3c2191168e915e201ec8c99b
free1139 <free1139@163.com>
2025-12-18 09:56:54 +0000

    fix install bin path

--- script/install_bin.sh
+++ script/install_bin.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+install_bin_path=/usr/local/bin
+install_etc_path=/etc/supd
+
 case "$1" in
     "install")
         if [ ! -f "../bin/supd" ]; then
@@ -8,23 +11,23 @@ case "$1" in
         fi
 
         mkdir -p /usr/bin
-        cp -rf ../bin/supd /usr/bin
-        cp -rf ../bin/supc /usr/bin
+        cp -rf ../bin/supd $install_bin_path
+        cp -rf ../bin/supc $install_bin_path
         mkdir -p /etc/supd/conf.d
-        cp -rf ../etc/supd/supd.ini /etc/supd
+        cp -rf ../etc/supd/supd.ini $install_etc_path
     ;;
     "upgrade")
         if [ ! -f "../bin/supd" ]; then
             echo "bin/supd is not exist."
             exit 1
         fi
-        cp -rf ../bin/supd /usr/bin
-        cp -rf ../bin/supc /usr/bin
+        cp -rf ../bin/supd $install_bin_path
+        cp -rf ../bin/supc $install_bin_path
     ;;
     "clean")
-        if [ -f "/usr/bin/supd" ];then
-            rm /usr/bin/supd
-            rm /usr/bin/supc
+        if [ -f "$install_bin_path/supd" ];then
+            rm $install_bin_path/supd
+            rm $install_bin_path/supc
         fi
     ;;
 esac
